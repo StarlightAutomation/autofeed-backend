60d7cc6541586d69ed50c7dae752317bdd248604
Nick <nick@agavenetworks.net>
2022-02-12 16:35:53 -0500

    remove base image on cleanup

--- build/scripts/4-build-cleanup.sh
+++ build/scripts/4-build-cleanup.sh
@@ -4,6 +4,10 @@ if [ -n ${PARTITION_TABLE} ]; then
   rm -f ./${PARTITION_TABLE}
 fi
 
+if [ -n ${BASE_IMAGE} ]; then
+  rm -f ${BASE_IMAGE}
+fi
+
 rm -f ./boot.tar
 rm -f ./root.tar
 rm -f ./custom-root.tar
