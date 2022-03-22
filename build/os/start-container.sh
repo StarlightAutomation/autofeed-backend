209cf4252b3576b466c06475f084fc815168afdf
Nick <nick@agavenetworks.net>
2022-03-22 17:09:17 -0400

    fix boot and update scripts add start-container script

---
+++ build/os/start-container.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+CONTAINER=$1
+
+if [[ $CONTAINER == "server" ]]; then
+  docker run \
+        --name autofeed-server \
+        --hostname autofeed-server \
+        --restart always \
+        --privileged \
+        -p 8080:8080 \
+        --mount type=bind,src=/etc/autofeed/data,dst=/etc/autofeed/data \
+        -d autofeed-server node .
+elif [[ $CONTAINER == "client" ]]; then
+  docker run \
+        --name autofeed-client \
+        --hostname autofeed-client \
+        --link autofeed-server \
+        --restart always \
+        -p 3000:3000 \
+        -d autofeed-client yarn start
+fi
+
+exit 0
--- build/os/update.sh
+++ build/os/update.sh
@@ -6,10 +6,18 @@ IMAGE=$2
 docker pull $IMAGE
 if [[ $APP == "server" ]]; then
     docker tag $IMAGE autofeed-server
-    docker restart autofeed-server
-elif [[ $APP === "client" ]]; then
+
+    docker stop autofeed-server
+    docker rm autofeed-server
+
+    . ./start-container.sh server
+elif [[ $APP == "client" ]]; then
     docker tag $IMAGE autofeed-client
-    docker restart autofeed-client
+
+    docker stop autofeed-client
+    docker rm autofeed-client
+
+    . ./start-container.sh client
 else
     echo "Invalid app '$APP'"
     exit 1
