4eee4b2f5047fef837bd75045100b5e0c61b64d1
nick <nickwilg@gmail.com>
2022-03-23 18:26:48 -0400

    cleanup, local env docker improvements

--- data/ap_config/iptables.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
--- data/init.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-systemctl daemon-reload
-
-systemctl enable autofeed-server.service
-systemctl enable autofeed-client.service
-
-rm -f /etc/autofeed/init.sh
-sleep 60
