010bd903a4d99e31ff16a5d0a37055ebb10f43a1
Nick <nick@agavenetworks.net>
2022-03-23 15:02:36 -0400

    add TZ env var based on host /etc/timezone setting

--- build/os/start-container.sh
+++ build/os/start-container.sh
@@ -2,6 +2,7 @@
 
 CONTAINER=$1
 
+TZ=$(cat /etc/timezone)
 if [[ $CONTAINER == "server" ]]; then
   docker run \
         --name autofeed-server \
@@ -9,6 +10,7 @@ if [[ $CONTAINER == "server" ]]; then
         --restart always \
         --privileged \
         -p 8080:8080 \
+        -e TZ=$TZ \
         --mount type=bind,src=/etc/autofeed/data,dst=/etc/autofeed/data \
         -d autofeed-server node .
 elif [[ $CONTAINER == "client" ]]; then
@@ -18,5 +20,6 @@ elif [[ $CONTAINER == "client" ]]; then
         --link autofeed-server \
         --restart always \
         -p 3000:3000 \
+        -e TZ=$TZ \
         -d autofeed-client yarn start
 fi
